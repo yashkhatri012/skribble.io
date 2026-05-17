7752dade5149dfb545782ca5c8978910a13425c1
Yash <yashkhatri88540@gmail.com>
2026-05-17 21:15:33 +0530

    railway port solved

--- server/server.js
+++ server/server.js
@@ -21,4 +21,7 @@ const server = http.createServer(app);
 initSocket(server);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
