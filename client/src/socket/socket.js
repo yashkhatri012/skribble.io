da0334689f4082ad5bb3a2b12ec39357a6dc1223
Yash <yashkhatri88540@gmail.com>
2026-05-17 20:56:57 +0530

    connection problem1

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+
+console.log("SOCKET URL:", import.meta.env.VITE_SOCKET_URL);
+export const socket = io(import.meta.env.VITE_SOCKET_URL, {
   autoConnect: false,
 });
