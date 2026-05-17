b097bd63b651ee3d69afb528b74810806ead234b
Yash <yashkhatri88540@gmail.com>
2026-05-17 21:06:07 +0530

    cors2

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 
-console.log("SOCKET URL:", import.meta.env.VITE_SOCKET_URL);
+
 export const socket = io(import.meta.env.VITE_SOCKET_URL, {
   autoConnect: false,
 });
